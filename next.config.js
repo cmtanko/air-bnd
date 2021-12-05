9e5ad7fd88f6e4dcc4ab3fdb510faf5035af7ff1
Suchan Badyakar <suchan211@gmail.com>
2021-12-05 19:37:29 +1100

    Update db connection string

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    DB_LOCAL_URL: "mongodb://localhost:27017/airbnd"
+    DB_LOCAL_URL: "mongodb://localhost:27017/airbnd",
+    DB_URL:
+      "mongodb+srv://myadmin:ycz6sATnTc3tvAQ@cluster0.sygo0.mongodb.net/airbnd?retryWrites=true&w=majority"
   }
 };
