f6143284620aa3ef1d10de00f5ffe3538619c5a6
Suchan Badyakar <suchan211@gmail.com>
2021-12-05 20:42:33 +1100

    [App-005] update cloud db (#7)
    * Update db connection string

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    DB_LOCAL_URL: "mongodb://localhost:27017/airbnd"
+    DB_LOCAL_URL: "mongodb://localhost:27017/airbnd",
+    ORIGIN_LOCAL: "http://localhost:3000",
+    ORIGIN: "https://airbnd-delta.vercel.app"
   }
 };
