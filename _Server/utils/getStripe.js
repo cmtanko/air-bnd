92a317e0b36462bce5e5b70c082d6d0540109f08
Suchan Badyakar <suchan211@gmail.com>
2021-12-24 18:44:46 +1100

    .

--- _Server/utils/getStripe.js
+++ _Server/utils/getStripe.js
@@ -3,6 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 
 const getStripe = () => {
+  alert(process.env.STRIPE_API_KEY);
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.STRIPE_API_KEY);
   }
