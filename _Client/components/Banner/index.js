ec5ee1083142a29a7dabc30b6b6ebb2115fea7f0
Suchan Badyakar <suchan211@gmail.com>
2022-01-16 11:00:12 +1100

    Update

--- _Client/components/Banner/index.js
+++ _Client/components/Banner/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Button } from "@mui/material";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Banner = () => {
+  const router = useRouter();
+
   return (
     <div className="relative h-[400px] sm:h-[400px] lg:h-[450px] xl:h-[500px] 2xl:h-[550px]">
       <Image
@@ -15,7 +18,12 @@ const Banner = () => {
         <p className="text-lg md:text-4xl sm:text-2xl text-white font-bold">
           Not sure where to go? Perfect.
         </p>
-        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-lg active:scale-90 transition duration-150">
+        <button
+          className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-lg active:scale-90 transition duration-150"
+          onClick={() => {
+            router.push("/rooms");
+          }}
+        >
           I'm flexible
         </button>
       </div>
